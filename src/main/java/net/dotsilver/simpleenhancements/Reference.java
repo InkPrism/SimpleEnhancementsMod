72821ed67c103058af70c17179db0a2fe573d370
InkPrism <contact@dotsilver.net>
2017-06-12 03:52:50 +0200

    Bump version

--- src/main/java/net/dotsilver/simpleenhancements/Reference.java
+++ src/main/java/net/dotsilver/simpleenhancements/Reference.java
@@ -3,7 +3,7 @@ package net.dotsilver.simpleenhancements;
 public class Reference {
 	public static final String MOD_ID = "simpleenhancements";
 	public static final String NAME = "§l§9Simple Enhancements Mod";
-	public static final String VERSION = "v0.7.2b by Dot_Silver";
+	public static final String VERSION = "v0.7.4a by Dot_Silver";
 	public static final String CLIENT_PROXY_CLASS = "net.dotsilver.simpleenhancements.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.dotsilver.simpleenhancements.proxy.CommonProxy";
 	public static final String DEPENDENCY = "";
